611e01dbc61cb7e9925733a386f078bc023975f4
Jordan Halterman <jordan.halterman@gmail.com>
2019-08-22 21:43:15 -0700

    Add license check to build.

--- Makefile
+++ Makefile
@@ -1,2 +1,28 @@
+.PHONY: build
+
+ATOMIX_K8S_CONTROLLER_VERSION := latest
+
+all: image
+
+build: # @HELP build the source code
 build:
-	go build ./...
+	go build -v ./...
+
+test: # @HELP run the unit tests and source code validation
+test: build deps license_check linters
+	go test github.com/atomix/atomix-go-client/pkg/...
+
+coverage: # @HELP generate unit test coverage data
+coverage: build deps linters license_check
+	./build/bin/coveralls-coverage
+
+deps: # @HELP ensure that the required dependencies are in place
+	go build -v ./...
+	bash -c "diff -u <(echo -n) <(git diff go.mod)"
+	bash -c "diff -u <(echo -n) <(git diff go.sum)"
+
+linters: # @HELP examines Go source code and reports coding problems
+	golangci-lint run
+
+license_check: # @HELP examine and ensure license headers exist
+	./build/licensing/boilerplate.py -v
--- pkg/client/map/map_test.go
+++ pkg/client/map/map_test.go
@@ -1,3 +1,17 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package _map
 
 import (
@@ -411,7 +425,7 @@ func TestMapOperations(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Nil(t, kv)
 
-	size, err := m.Size(context.Background())
+	size, err := m.Len(context.Background())
 	assert.NoError(t, err)
 	assert.Equal(t, 0, size)
 
@@ -427,7 +441,7 @@ func TestMapOperations(t *testing.T) {
 	assert.Equal(t, "bar", string(kv.Value))
 	version := kv.Version
 
-	size, err = m.Size(context.Background())
+	size, err = m.Len(context.Background())
 	assert.NoError(t, err)
 	assert.Equal(t, 1, size)
 
@@ -438,7 +452,7 @@ func TestMapOperations(t *testing.T) {
 	assert.Equal(t, "bar", string(kv.Value))
 	assert.Equal(t, version, kv.Version)
 
-	size, err = m.Size(context.Background())
+	size, err = m.Len(context.Background())
 	assert.NoError(t, err)
 	assert.Equal(t, 0, size)
 
@@ -460,7 +474,7 @@ func TestMapOperations(t *testing.T) {
 	err = m.Clear(context.Background())
 	assert.NoError(t, err)
 
-	size, err = m.Size(context.Background())
+	size, err = m.Len(context.Background())
 	assert.NoError(t, err)
 	assert.Equal(t, 0, size)
 
