e69bb860f5dcb57f44ceafbe607cdab4fd23deb5
Jordan Halterman <jordan.halterman@gmail.com>
2019-08-23 17:11:32 -0700

    Add tests to all exported packages.

---
+++ pkg/client/election/election_test.go
@@ -0,0 +1,36 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package election
+
+import (
+	"context"
+	"github.com/atomix/atomix-go-client/pkg/client/primitive"
+	"github.com/atomix/atomix-go-client/pkg/client/session"
+	"github.com/atomix/atomix-go-client/pkg/client/test"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestElectionOperations(t *testing.T) {
+	conns, partitions := test.StartTestPartitions(3)
+
+	name := primitive.NewName("default", "test", "default", "test")
+	election, err := New(context.TODO(), name, conns, session.WithTimeout(5*time.Second))
+	assert.NoError(t, err)
+	assert.NotNil(t, election)
+
+	test.StopTestPartitions(partitions)
+}
