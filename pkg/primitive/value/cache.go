91213b7a37fa2a1aebe58e3badbe3540e2737c4e
Jordan Halterman <jordan.halterman@gmail.com>
2023-02-01 22:25:04 -0800

    Update value cache with events

--- pkg/primitive/value/cache.go
+++ pkg/primitive/value/cache.go
@@ -6,84 +6,150 @@ package value
 
 import (
 	"context"
+	"github.com/atomix/atomix/api/errors"
 	"github.com/atomix/go-sdk/pkg/primitive"
+	"io"
 	"sync"
+	"time"
 )
 
 func newCachingValue[V any](value Value[V]) Value[V] {
-	return &cachingValue[V]{
+	cm := &cachingValue[V]{
 		Value: value,
 	}
+	cm.open(context.Background())
+	return cm
 }
 
 type cachingValue[V any] struct {
 	Value[V]
-	value *primitive.Versioned[V]
-	mu    sync.RWMutex
+	value  *primitive.Versioned[V]
+	mu     sync.RWMutex
+	cancel context.CancelFunc
 }
 
-func (m *cachingValue[V]) Set(ctx context.Context, value V, opts ...SetOption) (primitive.Versioned[V], error) {
-	versioned, err := m.Value.Set(ctx, value, opts...)
+func (v *cachingValue[V]) open(ctx context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	v.cancel = cancel
+	go v.watch(ctx)
+}
+
+func (v *cachingValue[V]) watch(ctx context.Context) {
+	events, err := v.Value.Events(ctx)
 	if err != nil {
-		m.mu.Lock()
-		m.value = nil
-		m.mu.Unlock()
+		time.AfterFunc(time.Second, func() {
+			v.watch(ctx)
+		})
+	}
+	for {
+		event, err := events.Next()
+		if errors.IsCanceled(err) {
+			return
+		}
+		if err == io.EOF {
+			time.AfterFunc(time.Second, func() {
+				v.watch(ctx)
+			})
+		}
+		if err != nil {
+			log.Error(err)
+		} else {
+			switch e := event.(type) {
+			case *Created[V]:
+				v.mu.Lock()
+				if v.value == nil || v.value.Version < e.Value.Version {
+					v.value = &e.Value
+				}
+				v.mu.Unlock()
+			case *Updated[V]:
+				v.mu.Lock()
+				if v.value == nil || v.value.Version < e.NewValue.Version {
+					v.value = &e.NewValue
+				}
+				v.mu.Unlock()
+			case *Deleted[V]:
+				v.mu.Lock()
+				if v.value == nil || v.value.Version <= e.Value.Version {
+					v.value = nil
+				}
+				v.mu.Unlock()
+			}
+		}
+	}
+}
+
+func (v *cachingValue[V]) Set(ctx context.Context, value V, opts ...SetOption) (primitive.Versioned[V], error) {
+	versioned, err := v.Value.Set(ctx, value, opts...)
+	if err != nil {
+		v.mu.Lock()
+		v.value = nil
+		v.mu.Unlock()
 		return versioned, err
 	}
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if m.value == nil || m.value.Version < versioned.Version {
-		m.value = &versioned
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	if v.value == nil || v.value.Version < versioned.Version {
+		v.value = &versioned
 	}
 	return versioned, err
 }
 
-func (m *cachingValue[V]) Update(ctx context.Context, value V, opts ...UpdateOption) (primitive.Versioned[V], error) {
-	versioned, err := m.Value.Update(ctx, value, opts...)
+func (v *cachingValue[V]) Update(ctx context.Context, value V, opts ...UpdateOption) (primitive.Versioned[V], error) {
+	versioned, err := v.Value.Update(ctx, value, opts...)
 	if err != nil {
-		m.mu.Lock()
-		m.value = nil
-		m.mu.Unlock()
+		v.mu.Lock()
+		v.value = nil
+		v.mu.Unlock()
 		return versioned, err
 	}
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if m.value == nil || m.value.Version < versioned.Version {
-		m.value = &versioned
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	if v.value == nil || v.value.Version < versioned.Version {
+		v.value = &versioned
 	}
 	return versioned, err
 }
 
-func (m *cachingValue[V]) Get(ctx context.Context, opts ...GetOption) (primitive.Versioned[V], error) {
-	m.mu.RLock()
-	value := m.value
-	m.mu.RUnlock()
+func (v *cachingValue[V]) Get(ctx context.Context, opts ...GetOption) (primitive.Versioned[V], error) {
+	v.mu.RLock()
+	value := v.value
+	v.mu.RUnlock()
 	if value != nil {
 		return *value, nil
 	}
 
-	versioned, err := m.Value.Get(ctx, opts...)
+	versioned, err := v.Value.Get(ctx, opts...)
 	if err != nil {
-		m.mu.Lock()
-		m.value = nil
-		m.mu.Unlock()
+		v.mu.Lock()
+		v.value = nil
+		v.mu.Unlock()
 		return versioned, err
 	}
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if m.value == nil || m.value.Version < versioned.Version {
-		m.value = &versioned
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	if v.value == nil || v.value.Version < versioned.Version {
+		v.value = &versioned
 	}
 	return versioned, err
 }
 
-func (m *cachingValue[V]) Delete(ctx context.Context, opts ...DeleteOption) error {
-	err := m.Value.Delete(ctx, opts...)
-	m.mu.Lock()
-	m.value = nil
-	m.mu.Unlock()
+func (v *cachingValue[V]) Delete(ctx context.Context, opts ...DeleteOption) error {
+	err := v.Value.Delete(ctx, opts...)
+	v.mu.Lock()
+	v.value = nil
+	v.mu.Unlock()
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func (v *cachingValue[V]) Close(ctx context.Context) error {
+	if err := v.Value.Close(ctx); err != nil {
+		return err
+	}
+	if v.cancel != nil {
+		v.cancel()
 	}
 	return nil
 }
